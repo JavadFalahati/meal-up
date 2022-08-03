8c916180ba6b85d37426bfdc8d489f7be033c023
jwdfhi <m.javadfalahati@gmail.com>
2022-08-03 20:37:58 +0430

    Initialize

---
+++ app/src/main/java/com/jwdfhi/meal_up/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.jwdfhi.meal_up
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jwdfhi.meal_up.ui.theme.Meal_upTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Meal_upTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Meal_upTheme {
+        Greeting("Android")
+    }
+}
