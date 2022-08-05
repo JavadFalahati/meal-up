0f1cdac2ad78dbf7da2041c58e2a993e5f9e2270
jwdfhi <m.javadfalahati@gmail.com>
2022-08-05 17:47:55 +0430

    Add context singleton

--- app/src/main/java/com/jwdfhi/meal_up/di/AppModule.kt
+++ app/src/main/java/com/jwdfhi/meal_up/di/AppModule.kt
@@ -1,10 +1,16 @@
 package com.jwdfhi.meal_up.di
 
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import com.jwdfhi.meal_up.databases.MealDao
+import com.jwdfhi.meal_up.databases.MealDatabase
 import com.jwdfhi.meal_up.services.MealService
 import com.jwdfhi.meal_up.utils.Constant
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,6 +20,10 @@ import javax.inject.Singleton
 @Module
 object AppModule {
 
+    @Singleton
+    @Provides
+    fun provideContext(application: Application): Context = application.applicationContext
+
     @Provides
     @Singleton
     fun provideMealService(): MealService {
@@ -22,6 +32,25 @@ object AppModule {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(MealService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMealDatabase(@ApplicationContext context: Context): MealDatabase {
+        return Room
+            .databaseBuilder(
+                context,
+                MealDatabase::class.java,
+                "meal_database"
+            )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMealDao(mealDatabase: MealDatabase): MealDao {
+        return mealDatabase.mealDao()
     }
 
 }
