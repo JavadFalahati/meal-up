5ee1d7270ff2071c3e637ae50da9ebd90cc0216f
jwdfhi <m.javadfalahati@gmail.com>
2022-08-05 17:49:40 +0430

    Add home hiltViewModel

--- app/src/main/java/com/jwdfhi/meal_up/screens/MealUpNavigation.kt
+++ app/src/main/java/com/jwdfhi/meal_up/screens/MealUpNavigation.kt
@@ -1,11 +1,13 @@
 package com.jwdfhi.meal_up.screens
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.jwdfhi.meal_up.screens.filter.FilterScreen
 import com.jwdfhi.meal_up.screens.home.HomeScreen
+import com.jwdfhi.meal_up.screens.home.HomeViewModel
 import com.jwdfhi.meal_up.screens.introduction.IntroductionScreen
 import com.jwdfhi.meal_up.screens.like.LikeScreen
 import com.jwdfhi.meal_up.screens.management.ManagementScreen
@@ -14,7 +16,7 @@ import com.jwdfhi.meal_up.screens.splash.SplashScreen
 
 
 @Composable
-fun ApplicationNavigation() {
+fun MealUpNavigation() {
     val navController = rememberNavController()
 
     NavHost(
@@ -25,8 +27,11 @@ fun ApplicationNavigation() {
             composable(
                 route = Screens.HomeScreen.name
             ) {
+                val homeViewModel = hiltViewModel<HomeViewModel>()
+
                 HomeScreen(
                     navController = navController,
+                    viewModel = homeViewModel
                 )
             }
 
