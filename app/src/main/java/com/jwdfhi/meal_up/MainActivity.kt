faa42ed3881c7fec0a55f480f852992c40aabe3e
jwdfhi <m.javadfalahati@gmail.com>
2022-08-06 22:58:59 +0430

    Add Navigation

--- app/src/main/java/com/jwdfhi/meal_up/MainActivity.kt
+++ app/src/main/java/com/jwdfhi/meal_up/MainActivity.kt
@@ -10,17 +10,22 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.jwdfhi.meal_up.screens.MealUpNavigation
 import com.jwdfhi.meal_up.ui.theme.Meal_upTheme
+import com.slaviboy.composeunits.initSize
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initSize()
         setContent {
             Meal_upTheme {
-                 // todo => add navigation
+                 MealUpNavigation()
             }
         }
+
     }
 }
