e3b23ce196dbff2cfd30acf4fe9c9a5a60977e1c
jwdfhi <m.javadfalahati@gmail.com>
2022-08-08 01:02:49 +0430

    Complete the logic of getRandomMeals method

--- app/src/main/java/com/jwdfhi/meal_up/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/jwdfhi/meal_up/screens/home/HomeViewModel.kt
@@ -1,27 +1,16 @@
 package com.jwdfhi.meal_up.screens.home
 
 import android.content.Context
-import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.jwdfhi.meal_up.models.DataOrException
-import com.jwdfhi.meal_up.models.LikedMealModel
-import com.jwdfhi.meal_up.models.MarkedMealModel
-import com.jwdfhi.meal_up.models.RandomMealServiceModel
+import com.jwdfhi.meal_up.models.*
 import com.jwdfhi.meal_up.repositories.MealDatabaseRepository
 import com.jwdfhi.meal_up.repositories.MealServiceRepository
-import com.jwdfhi.meal_up.utils.deviceHaveConnection
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
-import java.net.SocketException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -31,6 +20,12 @@ class HomeViewModel @Inject constructor(
     private val mealDatabaseRepository: MealDatabaseRepository
 ) : ViewModel() {
 
+    private var _randomMealsDataOrException:
+            MutableStateFlow<DataOrException<MutableList<RandomMealServiceModel.Meal>, Exception, DataOrExceptionStatus>> = MutableStateFlow(
+                DataOrException(status = DataOrExceptionStatus.Loading)
+            )
+    val randomMealsDataOrException = _randomMealsDataOrException.asStateFlow()
+
     private val _likedMealList = MutableStateFlow<List<LikedMealModel>>(emptyList())
     val likedMealList = _likedMealList.asStateFlow()
 
@@ -39,46 +34,90 @@ class HomeViewModel @Inject constructor(
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
-            mealDatabaseRepository.getLikedMeals().distinctUntilChanged().collect() {
-                _likedMealList.value = it
-            }
+//            mealDatabaseRepository.getLikedMeals().distinctUntilChanged().collect() {
+//                _likedMealList.value = it
+//            }
+//
+//            mealDatabaseRepository.getMarkedMeals().distinctUntilChanged().collect() {
+//                _markedMealList.value = it
+//            }
 
-            mealDatabaseRepository.getMarkedMeals().distinctUntilChanged().collect() {
-                _markedMealList.value = it
-            }
+            getRandomMeals()
         }
     }
 
-    suspend fun getRandomMeals(): DataOrException<MutableList<RandomMealServiceModel>, Boolean, Exception> {
-        if (!deviceHaveConnection(context)) { return DataOrException(exception = Exception(message = "Can't Connect to the Server!")) }
+    suspend fun getRandomMeals(): Unit {
+        _randomMealsDataOrException.value = DataOrException(status = DataOrExceptionStatus.Loading)
 
-        val mealList: MutableList<RandomMealServiceModel> = emptyList<RandomMealServiceModel>() as MutableList<RandomMealServiceModel>
+//        if (!deviceHaveConnection(context)) {
+//            _randomMealsDataOrException.value = DataOrException(
+//                exception = Exception(/*message = "Can't Connect to the Server!"*/),
+//                status = DataOrExceptionStatus.Failure
+//            )
+//            Log.d("ITS JAVAD", "exception two")
+//            return
+//        }
 
-        for (i in 0..10) {
-            val dataOrException: DataOrException<RandomMealServiceModel, Boolean, Exception>
-                = mealServiceRepository.getRandomMeal()
-            val exception: Exception? = dataOrException.exception
+        val mealList = mutableListOf<RandomMealServiceModel.Meal>()
 
-            if (exception != null) { return DataOrException(exception = exception) }
+        var repeatCount: Int = 10
+        while (repeatCount > 0) {
+            var isRepetitive = false
 
-            _likedMealList.value.forEach {
-                if (dataOrException.data!!.meals[0].idMeal == it.idMeal) {
-                    dataOrException.data!!.meals[0].isLiked = true
+            val dataOrException = getSingleRandomMeal()
+            if (dataOrException.status != DataOrExceptionStatus.Success) { return }
+
+            val id: String = dataOrException.data!!.meals[0].idMeal
+            mealList.forEach {
+                if (id == it.idMeal) {
+                    isRepetitive = true
+                    return@forEach
                 }
             }
 
-            _markedMealList.value.forEach {
-                if (dataOrException.data!!.meals[0].idMeal == it.idMeal) {
-                    dataOrException.data!!.meals[0].isMarked = true
-                    dataOrException.data!!.meals[0].markName = it.markName
-                    dataOrException.data!!.meals[0].markColor = it.markColor
-                }
+            if (!isRepetitive) {
+                repeatCount--
+                mealList.add(dataOrException.data!!.meals[0])
             }
-
-            mealList.add(dataOrException.data!!)
         }
 
-        return DataOrException(data = mealList)
+        _randomMealsDataOrException.value = DataOrException(
+            data = mealList,
+            status = DataOrExceptionStatus.Success
+        )
+    }
+
+    private suspend fun getSingleRandomMeal(): DataOrException<RandomMealServiceModel, Exception, DataOrExceptionStatus> {
+        var dataOrException = DataOrException<RandomMealServiceModel, Exception, DataOrExceptionStatus>()
+        dataOrException = mealServiceRepository.getRandomMeal()
+
+        val exception: Exception? = dataOrException.exception
+
+        if (exception != null) {
+            _randomMealsDataOrException.value = DataOrException(
+                exception = exception,
+                status = DataOrExceptionStatus.Failure
+            )
+            dataOrException.status = DataOrExceptionStatus.Failure
+            return dataOrException
+        }
+
+        _likedMealList.value.forEach {
+            if (dataOrException.data!!.meals[0].idMeal == it.idMeal) {
+                dataOrException.data!!.meals[0].isLiked = true
+            }
+        }
+
+        _markedMealList.value.forEach {
+            if (dataOrException.data!!.meals[0].idMeal == it.idMeal) {
+                dataOrException.data!!.meals[0].isMarked = true
+                dataOrException.data!!.meals[0].markName = it.markName
+                dataOrException.data!!.meals[0].markColor = it.markColor
+            }
+        }
+
+        dataOrException.status = DataOrExceptionStatus.Success
+        return dataOrException
     }
 
 }
