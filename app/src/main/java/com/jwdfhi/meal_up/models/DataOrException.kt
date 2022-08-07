92ed137c109ea9f1fa8a9a58d2bdc36aa4d6210c
jwdfhi <m.javadfalahati@gmail.com>
2022-08-07 23:34:51 +0430

    Add status field

--- app/src/main/java/com/jwdfhi/meal_up/models/DataOrException.kt
+++ app/src/main/java/com/jwdfhi/meal_up/models/DataOrException.kt
@@ -1,7 +1,10 @@
 package com.jwdfhi.meal_up.models
 
-data class DataOrException<T, Boolean, E: Exception>(
+import kotlinx.coroutines.flow.MutableStateFlow
+
+data class DataOrException<T, Boolean, E: Exception, DataOrExceptionStatus>(
     var data: T? = null,
     var loading: Boolean? = null,
     var exception: E? = null,
+    var status: DataOrExceptionStatus? = null
 )
