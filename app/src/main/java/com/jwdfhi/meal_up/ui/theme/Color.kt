b88c7bfe42560a9cdb11078a428b0991c5cf3e79
jwdfhi <m.javadfalahati@gmail.com>
2022-08-08 20:12:32 +0430

    Add new colors

--- app/src/main/java/com/jwdfhi/meal_up/ui/theme/Color.kt
+++ app/src/main/java/com/jwdfhi/meal_up/ui/theme/Color.kt
@@ -8,8 +8,16 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
 val PrimaryColor = Color(0xD3FF7C1C)
+val Primary80Color = Color(0xD3FA8F41)
+val Primary60Color = Color(0xD3FFA463)
 val LightPrimaryColor = Color(0xD3FFCCA7)
+
+val TransparentColor = Color.Transparent
 
 val Black90Color = Color(0xD31D1D1D)
 val Black80Color = Color(0xD3303030)
 val Black60Color = Color(0xD3666666)
+
+val White100Color = Color(0xD3FFFFFF)
+val White80Color = Color(0xD3E0E0E0)
+val White60Color = Color(0xD3A7A7A7)
