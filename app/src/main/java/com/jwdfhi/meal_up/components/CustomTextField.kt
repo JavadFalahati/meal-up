91cd4a804326224f99e303fc5badf19a54f6ff10
jwdfhi <m.javadfalahati@gmail.com>
2022-08-08 20:14:17 +0430

    Add errorText and keyboardActions for text

--- app/src/main/java/com/jwdfhi/meal_up/components/CustomTextField.kt
+++ app/src/main/java/com/jwdfhi/meal_up/components/CustomTextField.kt
@@ -1,18 +1,22 @@
 package com.jwdfhi.meal_up.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.jwdfhi.meal_up.ui.theme.PrimaryColor
@@ -26,46 +30,60 @@ fun CustomTextField(
     backgroundColor: Color,
     borderRadius: Dp = 8.dp,
     hint: String = "",
-    haveSearchIcon: Boolean = true,
-    searchOnTap: () -> Unit = {}
+    maxLength: Int = 200,
+    isError: Boolean = false,
+    errorText: (MutableState<String>) -> String = { "" },
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Done,
+    keyboardActions: KeyboardActions = KeyboardActions(),
+    leadingIcon: @Composable () -> Unit = {},
 ) {
-    TextField(
-        value = valueState.value,
-        onValueChange = { valueState.value = it },
-        modifier = Modifier
-            .padding(padding)
-//            .background(backgroundColor)
-            .fillMaxSize()
-            .clip(shape = RoundedCornerShape(borderRadius)),
-        enabled = enabled,
-        readOnly = readOnly,
-        singleLine = true,
-        colors = TextFieldDefaults.textFieldColors(
-            textColor = Color.Black,
-            backgroundColor = backgroundColor,
-            disabledTextColor = Color.Transparent,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent,
-        ),
-        placeholder = {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = valueState.value,
+            onValueChange = { if (valueState.value.length < maxLength) { valueState.value = it } },
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+                .clip(shape = RoundedCornerShape(borderRadius)),
+            enabled = enabled,
+            readOnly = readOnly,
+            singleLine = true,
+            isError = isError,
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.Black,
+                backgroundColor = backgroundColor,
+                disabledTextColor = Color.Transparent,
+                focusedIndicatorColor = if (isError) { Color.Red } else { Color.Transparent },
+                unfocusedIndicatorColor = if (isError) { Color.Red } else { Color.Transparent },
+                disabledIndicatorColor = Color.Transparent,
+                cursorColor = PrimaryColor
+            ),
+            placeholder = {
+                Text(
+                    text = hint,
+                    color = PrimaryColor
+                )
+            },
+            leadingIcon = leadingIcon,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType,
+                imeAction = imeAction,
+            ),
+            keyboardActions = keyboardActions
+        )
+        if (isError) {
+            Spacer(modifier = Modifier.height(4.dp))
             Text(
-                text = hint,
-                color = PrimaryColor
+                text = errorText(valueState),
+                style = TextStyle(
+                    color = Color.Red
+                )
             )
-        },
-        leadingIcon = {
-            if (haveSearchIcon) {
-                IconButton(
-                    onClick = { searchOnTap() }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Search,
-                        contentDescription = "Search",
-                        tint = PrimaryColor
-                    )
-                }
-            }
         }
-    )
+    }
+
 }
