055332eeaee29c35505d9b124746f68f315bbcc7
jwdfhi <m.javadfalahati@gmail.com>
2022-08-08 20:16:22 +0430

    Add systemUiController for statusBar color

--- app/src/main/java/com/jwdfhi/meal_up/screens/MealUpNavigation.kt
+++ app/src/main/java/com/jwdfhi/meal_up/screens/MealUpNavigation.kt
@@ -1,10 +1,13 @@
 package com.jwdfhi.meal_up.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.jwdfhi.meal_up.screens.filter.FilterScreen
 import com.jwdfhi.meal_up.screens.home.HomeScreen
 import com.jwdfhi.meal_up.screens.home.HomeViewModel
@@ -13,11 +16,17 @@ import com.jwdfhi.meal_up.screens.like.LikeScreen
 import com.jwdfhi.meal_up.screens.management.ManagementScreen
 import com.jwdfhi.meal_up.screens.mark.MarkScreen
 import com.jwdfhi.meal_up.screens.splash.SplashScreen
+import com.jwdfhi.meal_up.ui.theme.Primary60Color
+import com.jwdfhi.meal_up.ui.theme.Primary80Color
+import com.jwdfhi.meal_up.ui.theme.PrimaryColor
 
 
+@ExperimentalComposeUiApi
+@ExperimentalFoundationApi
 @Composable
 fun MealUpNavigation() {
     val navController = rememberNavController()
+    val systemUiController = rememberSystemUiController()
 
     NavHost(
         navController = navController,
@@ -29,6 +38,9 @@ fun MealUpNavigation() {
             ) {
                 val homeViewModel = hiltViewModel<HomeViewModel>()
 
+                systemUiController.setStatusBarColor(
+                    color = Primary80Color
+                )
                 HomeScreen(
                      navController = navController,
                      viewModel = homeViewModel
