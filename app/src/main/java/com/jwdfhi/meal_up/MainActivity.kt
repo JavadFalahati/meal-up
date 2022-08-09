90694b3d1ea6a9d7ee0b0e8c1963488701a4dfcb
jwdfhi <m.javadfalahati@gmail.com>
2022-08-10 01:29:39 +0430

    Add experimental annotation

--- app/src/main/java/com/jwdfhi/meal_up/MainActivity.kt
+++ app/src/main/java/com/jwdfhi/meal_up/MainActivity.kt
@@ -3,11 +3,13 @@ package com.jwdfhi.meal_up
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jwdfhi.meal_up.screens.MealUpNavigation
@@ -15,6 +17,8 @@ import com.jwdfhi.meal_up.ui.theme.Meal_upTheme
 import com.slaviboy.composeunits.initSize
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalComposeUiApi
+@ExperimentalFoundationApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
