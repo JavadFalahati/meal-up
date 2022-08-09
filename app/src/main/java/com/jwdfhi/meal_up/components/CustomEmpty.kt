a702790cdd577127b49f9868a5d1e060c2e20d80
jwdfhi <m.javadfalahati@gmail.com>
2022-08-10 01:26:09 +0430

    Implement CustomEmpty.kt for empty composable

---
+++ app/src/main/java/com/jwdfhi/meal_up/components/CustomEmpty.kt
@@ -0,0 +1,63 @@
+package com.jwdfhi.meal_up.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.jwdfhi.meal_up.R
+import com.slaviboy.composeunits.dh
+import com.slaviboy.composeunits.dw
+import com.slaviboy.composeunits.sh
+
+@Composable
+fun CustomEmpty(
+    showTitle: Boolean = false
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .width(0.6.dw)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            val painter = painterResource(id = R.drawable.empty_icon_1)
+            Image(
+                modifier = Modifier
+                    .aspectRatio(ratio = painter.intrinsicSize.height / painter.intrinsicSize.width)
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                painter = painter,
+                contentDescription = "Empty",
+                contentScale = ContentScale.Fit
+            )
+        }
+        if (showTitle) {
+            Box(
+                modifier = Modifier
+                    .width(width = 0.6.dw)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = "No content to show",
+                    style = TextStyle(
+                        fontSize = 0.020.sh,
+                        textAlign = TextAlign.Center,
+                    ),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(0.1.dh))
+    }
+}
