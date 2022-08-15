341502f5b97aec530ef54c6d708d420dae8c05a5
jwdfhi <m.javadfalahati@gmail.com>
2022-08-15 20:37:22 +0430

    Change the items type to mutableStateList

--- app/src/main/java/com/jwdfhi/meal_up/models/FilterItemModel.kt
+++ app/src/main/java/com/jwdfhi/meal_up/models/FilterItemModel.kt
@@ -1,8 +1,12 @@
 package com.jwdfhi.meal_up.models
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.snapshots.SnapshotStateList
+
 data class FilterItemModel<T> (
     val title: String,
-    val items: List<T>,
+    val items: SnapshotStateList<T>,
+    val selectItem: (item: T) -> Unit,
     val clearItem: (item: T) -> Unit,
     val clearAllItems: () -> Unit,
 )
