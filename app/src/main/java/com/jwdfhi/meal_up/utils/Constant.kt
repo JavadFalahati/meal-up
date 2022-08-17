9adf9474cf5d49b40fbf1205cd35c17ee38e7f67
jwdfhi <m.javadfalahati@gmail.com>
2022-08-18 00:43:39 +0430

    Add new argumentKey

--- app/src/main/java/com/jwdfhi/meal_up/utils/Constant.kt
+++ app/src/main/java/com/jwdfhi/meal_up/utils/Constant.kt
@@ -2,6 +2,8 @@ package com.jwdfhi.meal_up.utils
 
 object Constant {
 
-    const val MEAL_SERVICE_BASE_ENDPOINT: String = "https://www.themealdb.com/api/json/v1/1/";
+    const val MEAL_SERVICE_BASE_ENDPOINT: String = "https://www.themealdb.com/api/json/v1/1/"
+
+    const val FILTERS_ARGUMENT_KEY: String = "FiltersArgumentKey"
 
 }
