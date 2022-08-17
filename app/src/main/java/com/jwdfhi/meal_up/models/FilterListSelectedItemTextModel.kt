61a0d2302a7fc830e1ae7edbafd451958b931422
jwdfhi <m.javadfalahati@gmail.com>
2022-08-18 00:48:35 +0430

    Implement FilteredMealModel.kt and FilterListSelectedItemTextModel.kt models

---
+++ app/src/main/java/com/jwdfhi/meal_up/models/FilterListSelectedItemTextModel.kt
@@ -0,0 +1,11 @@
+package com.jwdfhi.meal_up.models
+
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FilterListSelectedItemTextModel(
+    var category: String = "",
+    var ingredients: MutableList<String> = mutableListOf(),
+    var area: String = "",
+)
---
+++ app/src/main/java/com/jwdfhi/meal_up/models/FilteredMealModel.kt
@@ -0,0 +1,7 @@
+package com.jwdfhi.meal_up.models
+
+data class FilteredMealModel(
+    val idMeal: String,
+    val strMeal: String,
+    val strMealThumb: String
+)
