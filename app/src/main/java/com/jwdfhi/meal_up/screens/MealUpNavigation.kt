5fb71675830f580ac23b36720b6e8ebf81ea0630
jwdfhi <m.javadfalahati@gmail.com>
2022-08-18 00:44:51 +0430

    Add arguments for FilterScreen to getting data from last screen

--- app/src/main/java/com/jwdfhi/meal_up/screens/MealUpNavigation.kt
+++ app/src/main/java/com/jwdfhi/meal_up/screens/MealUpNavigation.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.jwdfhi.meal_up.models.FilterListSelectedItemTextModel
 import com.jwdfhi.meal_up.screens.filter.FilterScreen
 import com.jwdfhi.meal_up.screens.filter.FilterViewModel
 import com.jwdfhi.meal_up.screens.home.HomeScreen
@@ -18,6 +21,10 @@ import com.jwdfhi.meal_up.screens.management.ManagementScreen
 import com.jwdfhi.meal_up.screens.mark.MarkScreen
 import com.jwdfhi.meal_up.screens.splash.SplashScreen
 import com.jwdfhi.meal_up.ui.theme.*
+import com.jwdfhi.meal_up.utils.Constant
+import kotlinx.serialization.DeserializationStrategy
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
 
 @ExperimentalComposeUiApi
@@ -35,29 +42,61 @@ fun MealUpNavigation() {
             composable(
                 route = Screens.HomeScreen.name
             ) {
-                val homeViewModel = hiltViewModel<HomeViewModel>()
-
                 systemUiController.setStatusBarColor(
                     color = Primary80Color
                 )
+
+                val homeViewModel = hiltViewModel<HomeViewModel>()
+
+                var filterListSelectedItemTextModel: FilterListSelectedItemTextModel = FilterListSelectedItemTextModel()
+                if (navController.currentBackStackEntry!!.savedStateHandle.contains(Constant.FILTERS_ARGUMENT_KEY)) {
+                    val filterListSelectedItemTextModelNullable = navController.currentBackStackEntry!!.savedStateHandle
+                        .get<String>(Constant.FILTERS_ARGUMENT_KEY)
+
+                    if (filterListSelectedItemTextModelNullable != null) {
+                        filterListSelectedItemTextModel =
+                            Json.decodeFromString<FilterListSelectedItemTextModel>(filterListSelectedItemTextModelNullable)
+                    }
+                }
+
                 HomeScreen(
                      navController = navController,
-                     viewModel = homeViewModel
+                     viewModel = homeViewModel,
+                     filterListSelectedItemTextModel = filterListSelectedItemTextModel
                 )
             }
 
             composable(
-                route = Screens.FilterScreen.name
-            ) {
-                val filterViewModel = hiltViewModel<FilterViewModel>()
-
+                route = Screens.FilterScreen.name + "/{${Constant.FILTERS_ARGUMENT_KEY}}",
+                arguments = listOf(
+                    navArgument(
+                        name = Constant.FILTERS_ARGUMENT_KEY,
+                        builder = {
+                            type = NavType.StringType
+                        }
+                    )
+                )
+            ) { navBackStackEntry ->
                 systemUiController.setStatusBarColor(
                     color = White80Color
                 )
-                FilterScreen(
-                    navController = navController,
-                    viewModel = filterViewModel
-                )
+
+                val filterViewModel = hiltViewModel<FilterViewModel>()
+
+                var filterListSelectedItemTextModel = FilterListSelectedItemTextModel()
+
+                navBackStackEntry.arguments?.getString(Constant.FILTERS_ARGUMENT_KEY).let {
+                    if (it != null) {
+                        filterListSelectedItemTextModel = Json.decodeFromString<FilterListSelectedItemTextModel>(it)
+                    }
+
+                    FilterScreen(
+                        navController = navController,
+                        viewModel = filterViewModel,
+                        filterListSelectedItemTextModel = filterListSelectedItemTextModel
+                    )
+                }
+
             }
 
             composable(
