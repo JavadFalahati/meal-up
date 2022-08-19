d6565ce3abae8b7de7b83adb8e017541cef44895
jwdfhi <m.javadfalahati@gmail.com>
2022-08-19 21:58:45 +0430

    Add meal Id Argument Key

--- app/src/main/java/com/jwdfhi/meal_up/utils/Constant.kt
+++ app/src/main/java/com/jwdfhi/meal_up/utils/Constant.kt
@@ -5,5 +5,6 @@ object Constant {
     const val MEAL_SERVICE_BASE_ENDPOINT: String = "https://www.themealdb.com/api/json/v1/1/"
 
     const val FILTERS_ARGUMENT_KEY: String = "FiltersArgumentKey"
+    const val MEAL_ID_ARGUMENT_KEY: String = "MealIdArgumentKey"
 
 }
