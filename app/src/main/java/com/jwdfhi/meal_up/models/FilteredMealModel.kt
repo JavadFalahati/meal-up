7b27a22ed1e1cb213a457f1e0103bc331ea56a3b
jwdfhi <m.javadfalahati@gmail.com>
2022-08-19 17:36:48 +0430

    Add like and marked property to the model

--- app/src/main/java/com/jwdfhi/meal_up/models/FilteredMealModel.kt
+++ app/src/main/java/com/jwdfhi/meal_up/models/FilteredMealModel.kt
@@ -3,5 +3,11 @@ package com.jwdfhi.meal_up.models
 data class FilteredMealModel(
     val idMeal: String,
     val strMeal: String,
-    val strMealThumb: String
+    val strMealThumb: String,
+
+    var isLiked: Boolean = false,
+
+    var isMarked: Boolean = false,
+    var markColor: String = "",
+    var markName: String = "",
 )
