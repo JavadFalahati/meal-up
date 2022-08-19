9578c1c6e489634255216fc8ecd816c006d0b62c
jwdfhi <m.javadfalahati@gmail.com>
2022-08-19 17:38:28 +0430

    Add haveSelectedItem property value and Refactor names

--- app/src/main/java/com/jwdfhi/meal_up/screens/filter/FilterScreen.kt
+++ app/src/main/java/com/jwdfhi/meal_up/screens/filter/FilterScreen.kt
@@ -1,52 +1,35 @@
 package com.jwdfhi.meal_up.screens.filter
 
-import android.util.Log
-import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.google.accompanist.swiperefresh.SwipeRefresh
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.jwdfhi.meal_up.components.*
 import com.jwdfhi.meal_up.models.*
 import com.jwdfhi.meal_up.screens.filter.components.FilterAppbar
 import com.jwdfhi.meal_up.screens.filter.components.FilterItem
-import com.jwdfhi.meal_up.ui.theme.Black60Color
 import com.jwdfhi.meal_up.ui.theme.GreyBackgroundScreen
-import com.jwdfhi.meal_up.ui.theme.PrimaryColor
 import com.jwdfhi.meal_up.ui.theme.White100Color
 import com.slaviboy.composeunits.dh
-import com.slaviboy.composeunits.dw
 import com.slaviboy.composeunits.sh
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @Composable
 fun FilterScreen(
     navController: NavController,
     viewModel: FilterViewModel,
-    filterListSelectedItemTextModel: FilterListSelectedItemTextModel
+    filterListSelectedItemModel: FilterListSelectedItemModel
 ) {
     LaunchedEffect(Unit) {
-        viewModel.initState(filterListSelectedItemTextModel = filterListSelectedItemTextModel)
+        viewModel.initState(filterListSelectedItemModel = filterListSelectedItemModel)
     }
 
     CustomBackPressHandler(onBackPressed = { navController.popBackStack() })
@@ -66,11 +49,11 @@ fun FilterScreen(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            when (viewModel.initStateDataOrException.collectAsState().value.status) {
+            when (viewModel.filtersDataOrException.collectAsState().value.status) {
                 DataOrExceptionStatus.Loading -> CustomLoading(loadingType = LoadingType.Linear, title = "")
                 DataOrExceptionStatus.Failure -> CustomError(
-                    title = viewModel.initStateDataOrException.collectAsState().value.exception!!.message!!,
-                    tryAgainOnTap = { viewModel.initState(filterListSelectedItemTextModel = filterListSelectedItemTextModel) }
+                    title = viewModel.filtersDataOrException.collectAsState().value.exception!!.message!!,
+                    tryAgainOnTap = { viewModel.initState(filterListSelectedItemModel = filterListSelectedItemModel) }
                 )
                 DataOrExceptionStatus.Success -> {
                     Column(
@@ -107,8 +90,9 @@ fun FilterScreen(
                                                 selectItem = { viewModel.selectSingleStateOfFilter<MealCategoryListServiceModel.Category>(it) },
                                                 clearItem = { viewModel.clearSingleStateOfFilter<MealCategoryListServiceModel.Category>(it) },
                                                 clearAllItems = { viewModel.clearAllStateOfFilter(FilterType.Category) },
-                                                items = viewModel.initStateDataOrException.collectAsState().value.data!!.categories
+                                                items = viewModel.filtersDataOrException.collectAsState().value.data!!.categories
                                             ),
+                                            haveSelectedItem = viewModel.filterListSelectedItemModel.category.isNotEmpty(),
                                             height = 0.2.dh,
                                             verticalMargin = 0.02.dh,
                                             horizontalMargin = 6.dp
@@ -120,8 +104,9 @@ fun FilterScreen(
                                                 selectItem = { viewModel.selectSingleStateOfFilter<MealIngredientListServiceModel.Meal>(it) },
                                                 clearItem = { viewModel.clearSingleStateOfFilter<MealIngredientListServiceModel.Meal>(it) },
                                                 clearAllItems = { viewModel.clearAllStateOfFilter(FilterType.Ingredient) },
-                                                items = viewModel.initStateDataOrException.collectAsState().value.data!!.ingredients
+                                                items = viewModel.filtersDataOrException.collectAsState().value.data!!.ingredients
                                             ),
+                                            haveSelectedItem = viewModel.filterListSelectedItemModel.ingredients.isNotEmpty(),
                                             height = 0.2.dh,
                                             verticalMargin = 0.02.dh,
                                             horizontalMargin = 6.dp
@@ -133,8 +118,9 @@ fun FilterScreen(
                                                 selectItem = { viewModel.selectSingleStateOfFilter<MealAreaListServiceModel.Meal>(it) },
                                                 clearItem = { viewModel.clearSingleStateOfFilter<MealAreaListServiceModel.Meal>(it) },
                                                 clearAllItems = { viewModel.clearAllStateOfFilter(FilterType.Area) },
-                                                items = viewModel.initStateDataOrException.collectAsState().value.data!!.areas
+                                                items = viewModel.filtersDataOrException.collectAsState().value.data!!.areas
                                             ),
+                                            haveSelectedItem = viewModel.filterListSelectedItemModel.area.isNotEmpty(),
                                             height = 0.2.dh,
                                             verticalMargin = 0.02.dh,
                                             horizontalMargin = 6.dp
