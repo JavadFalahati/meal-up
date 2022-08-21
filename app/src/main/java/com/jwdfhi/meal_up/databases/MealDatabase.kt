cecef3682d81314b10ff66f7c8fc85225e0b157d
jwdfhi <m.javadfalahati@gmail.com>
2022-08-21 22:02:07 +0430

    Change the entity and version

--- app/src/main/java/com/jwdfhi/meal_up/databases/MealDatabase.kt
+++ app/src/main/java/com/jwdfhi/meal_up/databases/MealDatabase.kt
@@ -4,8 +4,9 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.jwdfhi.meal_up.models.LikedMealModel
 import com.jwdfhi.meal_up.models.MarkedMealModel
+import com.jwdfhi.meal_up.models.MealModel
 
-@Database(entities = [LikedMealModel::class, MarkedMealModel::class], version = 1, exportSchema = false)
+@Database(entities = [MealModel::class], version = 2, exportSchema = false)
 abstract class MealDatabase : RoomDatabase() {
     abstract fun mealDao(): MealDao
 }
