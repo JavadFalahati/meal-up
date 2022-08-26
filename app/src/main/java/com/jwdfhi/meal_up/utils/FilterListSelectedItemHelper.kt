de5c8847a265b42e3091554fc4c1fee2ce86e108
jwdfhi <m.javadfalahati@gmail.com>
2022-08-26 20:12:32 +0430

    Remove FilterListSelectedItemHelper class and Add two methods on file

--- app/src/main/java/com/jwdfhi/meal_up/utils/FilterListSelectedItemHelper.kt
+++ app/src/main/java/com/jwdfhi/meal_up/utils/FilterListSelectedItemHelper.kt
@@ -3,33 +3,24 @@ package com.jwdfhi.meal_up.utils
 import com.jwdfhi.meal_up.models.FilterListSelectedItemModel
 import com.jwdfhi.meal_up.models.FilterType
 
-class FilterListSelectedItemHelper {
-
-    companion object {
-
-        public fun notCheckingPreviousFilteredList(
-            filterType: FilterType,
-            filterListSelectedItemModel: FilterListSelectedItemModel
-        ): Boolean {
-            when (filterType) {
-                FilterType.Category -> return true
-                FilterType.Ingredient -> if (filterListSelectedItemModel.category.isEmpty()) { return true }
-                FilterType.Area -> {
-                    if (filterListSelectedItemModel.category.isEmpty()
-                        && filterListSelectedItemModel.ingredients.isEmpty()
-                    ) { return true }
-                }
-            }
-
-            return false
+public fun FilterListSelectedItemModel.notCheckingPreviousFilteredList(
+    filterType: FilterType,
+): Boolean {
+    when (filterType) {
+        FilterType.Category -> return true
+        FilterType.Ingredient -> if (this.category.isEmpty()) { return true }
+        FilterType.Area -> {
+            if (this.category.isEmpty()
+                && this.ingredients.isEmpty()
+            ) { return true }
         }
-
-        public fun isNotEmpty(filterListSelectedItemModel: FilterListSelectedItemModel): Boolean {
-            return  filterListSelectedItemModel.category.isNotEmpty()
-                    || filterListSelectedItemModel.ingredients.isNotEmpty()
-                    || filterListSelectedItemModel.area.isNotEmpty()
-        }
-
     }
 
+    return false
+}
+
+public fun FilterListSelectedItemModel.isNotEmpty(): Boolean {
+    return  this.category.isNotEmpty()
+            || this.ingredients.isNotEmpty()
+            || this.area.isNotEmpty()
 }
