8f29fe4b257d267eef8618516b0f9f8542b1c080
jwdfhi <m.javadfalahati@gmail.com>
2022-08-26 17:29:00 +0430

    make value property nullable

--- app/src/main/java/com/jwdfhi/meal_up/models/IngredientWithColorModel.kt
+++ app/src/main/java/com/jwdfhi/meal_up/models/IngredientWithColorModel.kt
@@ -3,6 +3,6 @@ package com.jwdfhi.meal_up.models
 import androidx.compose.ui.graphics.Color
 
 data class IngredientWithColorModel(
-    val value: String,
+    val value: String?,
     val color: Color
 )
