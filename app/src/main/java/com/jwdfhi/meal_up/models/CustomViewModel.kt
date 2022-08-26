a6997bf073470436075ca998553c9c20ebc845c2
jwdfhi <m.javadfalahati@gmail.com>
2022-08-27 00:34:18 +0430

    Add initState with no parameter method

--- app/src/main/java/com/jwdfhi/meal_up/models/CustomViewModel.kt
+++ app/src/main/java/com/jwdfhi/meal_up/models/CustomViewModel.kt
@@ -2,10 +2,12 @@ package com.jwdfhi.meal_up.models
 
 import androidx.navigation.NavController
 
-interface CustomViewModel<InitStateFirstEntryType, InitStateSecondEntryType> {
+interface CustomViewModel<InitStateFirstEntryType, InitStateSecondEntryType, InitStateThirdEntryType> {
 
+    fun initState(): Unit {}
     fun initState(entry: InitStateFirstEntryType): Unit {}
     fun initState(entryOne: InitStateFirstEntryType, entryTwo: InitStateSecondEntryType): Unit {}
+    fun initState(entryOne: InitStateFirstEntryType, entryTwo: InitStateSecondEntryType, entryThree: InitStateThirdEntryType): Unit {}
 
     fun onBackPressed(navController: NavController): Unit
 
