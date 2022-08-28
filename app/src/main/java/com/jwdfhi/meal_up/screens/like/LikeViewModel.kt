b5936dd419f3543bc2543d9ba93eb8421be1a4fe
jwdfhi <m.javadfalahati@gmail.com>
2022-08-29 00:01:09 +0430

    Implement init block for getting data from database

--- app/src/main/java/com/jwdfhi/meal_up/screens/like/LikeViewModel.kt
+++ app/src/main/java/com/jwdfhi/meal_up/screens/like/LikeViewModel.kt
@@ -1,6 +1,8 @@
 package com.jwdfhi.meal_up.screens.like
 
 import android.content.Context
+import android.util.Log
+import androidx.compose.ui.text.toLowerCase
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
@@ -11,9 +13,13 @@ import com.jwdfhi.meal_up.models.MealModel
 import com.jwdfhi.meal_up.repositories.MealDatabaseRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,17 +28,17 @@ class LikeViewModel @Inject constructor(
     private val mealDatabaseRepository: MealDatabaseRepository
 ) : ViewModel(), CustomViewModel<Nothing, Nothing, Nothing> {
 
+     init {
+         getLikedMeals()
+     }
+
     override fun initState() {
-        viewModelScope.launch(Dispatchers.IO) {
-            _mealsDataOrException.value = DataOrException(status = DataOrExceptionStatus.Loading)
+        _mealsDataOrException.value = DataOrException(status = DataOrExceptionStatus.Loading)
 
-            val likedMeals = getLikedMeals()
-
-            _mealsDataOrException.value = DataOrException(
-                data = likedMeals,
-                status = DataOrExceptionStatus.Success
-            )
-        }
+        _mealsDataOrException.value = DataOrException(
+            data = _storedMealList.value.toMutableList(),
+            status = DataOrExceptionStatus.Success
+        )
     }
 
     override fun onBackPressed(navController: NavController) {
@@ -43,41 +49,41 @@ class LikeViewModel @Inject constructor(
         DataOrException(status = DataOrExceptionStatus.Loading)
     )
     val mealsDataOrException = _mealsDataOrException.asStateFlow()
-    private suspend fun getLikedMeals(): MutableList<MealModel> {
-        var likedMeals = emptyList<MealModel>()
-        mealDatabaseRepository.getMeals().collect {
-            likedMeals = it
-        }
 
-        return likedMeals.toMutableList()
+    private val _storedMealList = MutableStateFlow<List<MealModel>>(emptyList())
+    private fun getLikedMeals() {
+        viewModelScope.launch(Dispatchers.IO) {
+            mealDatabaseRepository.getMeals().collect {
+                _storedMealList.value = it.filter { storedMeal -> storedMeal.isLiked }
+                return@collect
+            }
+        }
     }
 
     fun searchMealByName(value: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            _mealsDataOrException.value = DataOrException(status = DataOrExceptionStatus.Loading)
+        val existedMealList = _mealsDataOrException.value.data
 
-            val likedMeals = getLikedMeals()
+        _mealsDataOrException.value = DataOrException(status = DataOrExceptionStatus.Loading)
 
-            if (value.trim().isEmpty()) {
-                _mealsDataOrException.value = DataOrException(
-                    data = likedMeals,
-                    status = DataOrExceptionStatus.Success
-                )
-                return@launch
-            }
-
-            val filteredByNameMeals = mutableListOf<MealModel>()
-            _mealsDataOrException.value.data?.forEach { storedMeal ->
-                if (storedMeal.strMeal.contains(value.trim())) {
-                    filteredByNameMeals.add(storedMeal)
-                }
-            }
-
+        if (value.trim().isEmpty()) {
             _mealsDataOrException.value = DataOrException(
-                data = filteredByNameMeals,
+                data = _storedMealList.value.toMutableList(),
                 status = DataOrExceptionStatus.Success
             )
+            return
         }
+
+        val filteredByNameMeals = mutableListOf<MealModel>()
+        existedMealList?.forEach { storedMeal ->
+            if (storedMeal.strMeal.lowercase(Locale.getDefault()).contains(value.lowercase(Locale.getDefault()))) {
+                filteredByNameMeals.add(storedMeal)
+            }
+        }
+
+        _mealsDataOrException.value = DataOrException(
+            data = filteredByNameMeals,
+            status = DataOrExceptionStatus.Success
+        )
     }
 
 }
