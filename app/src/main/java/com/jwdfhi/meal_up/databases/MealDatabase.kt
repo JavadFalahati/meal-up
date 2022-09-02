8233c4618632efdbc3c11e28a9c45e25943a7572
jwdfhi <m.javadfalahati@gmail.com>
2022-09-02 23:14:29 +0430

    Add typeConverter and Reversion

--- app/src/main/java/com/jwdfhi/meal_up/databases/MealDatabase.kt
+++ app/src/main/java/com/jwdfhi/meal_up/databases/MealDatabase.kt
@@ -2,11 +2,14 @@ package com.jwdfhi.meal_up.databases
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.jwdfhi.meal_up.models.LikedMealModel
 import com.jwdfhi.meal_up.models.MarkedMealModel
 import com.jwdfhi.meal_up.models.MealModel
+import com.jwdfhi.meal_up.utils.MealDatabaseConverter
 
-@Database(entities = [MealModel::class], version = 6, exportSchema = false)
+@Database(entities = [MealModel::class], version = 7, exportSchema = false)
+@TypeConverters(MealDatabaseConverter::class)
 abstract class MealDatabase : RoomDatabase() {
     abstract fun mealDao(): MealDao
 }
