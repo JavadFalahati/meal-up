4f50c8603d9843dfdc8e02fbe231203a1d5eafc0
jwdfhi <m.javadfalahati@gmail.com>
2022-09-05 21:49:26 +0430

    Add two extension getName and getColor method in MealCategory.kt

--- app/src/main/java/com/jwdfhi/meal_up/models/MealCategory.kt
+++ app/src/main/java/com/jwdfhi/meal_up/models/MealCategory.kt
@@ -1,7 +1,22 @@
 package com.jwdfhi.meal_up.models
 
+import androidx.compose.ui.graphics.Color
+import com.jwdfhi.meal_up.ui.theme.*
+
 enum class MealCategory {
     BREAKFAST,
     DINNER,
     LAUNCH
+}
+
+public fun MealCategory.getName(): String = when (this) {
+    MealCategory.BREAKFAST -> "Breakfast"
+    MealCategory.LAUNCH -> "Launch"
+    MealCategory.DINNER -> "Dinner"
+}
+
+public fun MealCategory.getColor(): Color = when (this) {
+    MealCategory.BREAKFAST -> Green80Color
+    MealCategory.LAUNCH -> Yellow80Color
+    MealCategory.DINNER -> Red80Color
 }
