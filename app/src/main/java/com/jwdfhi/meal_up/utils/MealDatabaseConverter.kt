6c6b8a68df71b5edb41e7466d6dd579580a9bc25
jwdfhi <m.javadfalahati@gmail.com>
2022-09-05 21:48:14 +0430

    Refactor mealCategory converters

--- app/src/main/java/com/jwdfhi/meal_up/utils/MealDatabaseConverter.kt
+++ app/src/main/java/com/jwdfhi/meal_up/utils/MealDatabaseConverter.kt
@@ -1,18 +1,19 @@
 package com.jwdfhi.meal_up.utils
 
+import androidx.compose.ui.graphics.Color
 import androidx.room.TypeConverter
 import com.jwdfhi.meal_up.models.MealCategory
 
 class MealDatabaseConverter {
 
     @TypeConverter
-    fun fromMealCategory(mealCategory: MealCategory?): String {
-        return mealCategory?.name ?: ""
+    fun fromMealCategory(mealCategory: MealCategory?): String? {
+        return if (mealCategory != null) mealCategory.name else null
     }
 
     @TypeConverter
-    fun toMealCategory(mealCategory: String): MealCategory? {
-        return if (mealCategory.isEmpty()) null else MealCategory.valueOf(mealCategory)
+    fun toMealCategory(mealCategory: String?): MealCategory? {
+        return if (mealCategory != null) MealCategory.valueOf(mealCategory) else null
     }
-
 }
+
